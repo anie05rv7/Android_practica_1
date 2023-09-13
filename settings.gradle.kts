9fe367b8b93ab7c01897729d90c50c96f4dae7d7
ANGELES <anie05rv7@gmail.com>
2023-09-12 20:53:25 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practica 1 12.09.2023"
+include(":app")
+ 
